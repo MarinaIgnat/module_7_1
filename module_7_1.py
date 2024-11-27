a781fdb0ed49c8535fa832928f14497c7d0b794e
MarinaIgnat <9702m@mail.ru>
2024-11-27 20:50:20 +0700

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -10,27 +10,30 @@ class Product:
         return f'{self.name}, {self.weight}, {self.category}'
 
 
-class Shop():
-    __file_name = 'products.txt'
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
 
     def get_products(self):
-        open(self.__file_name)
-        self.__file_name.read()
-        return f'{self.__file_name}'
-        self.__file_name.close()
+        self.__file_name = 'products.txt'
+        file = open(self.__file_name, 'r')
+        products = file.read()
+        file.close()
+
+        return products
 
 
-    def add(self, *products, name):
-        self.products = products
-        super.__init__(name)
-        if name in self.products:
-            return f'Продукт {name} уже есть в магазине.'
-        else:
-            self.__file_name.__add__(self.products)
+    def add(self, *products):
+        for product in products:
+            if product.name not in self.get_products():
+                file = open(self.__file_name, 'a')
+                file.write(product.__str__() + '\n')
+            else:
+                return f'Продукт {product.name} уже есть в магазине.'
 
 
 s1 = Shop()
-p1 = Product('Potato', 50.5, 'Vegetables')    #Почему не выводятся р1, р2, р3?
+p1 = Product('Potato', 50.5, 'Vegetables')    
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
